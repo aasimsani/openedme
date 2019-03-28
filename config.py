657b2066d22e5ac47c06ddfc3e24d32b882dcfd0
Aasim Sani <aasimsani@legolas.local>
2019-03-28 15:56:13 -0400

    Changed port to run on

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-
+# Configure this file to make the application work
 DOMAIN_NAME = "localhost:5000"
 MJ_APIKEY_PRIVATE = ""
 MJ_APIKEY_PUBLIC = ""
--- opened_me.py
+++ opened_me.py
@@ -57,4 +57,4 @@ def redirect_to():
     return redirect(link)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=80)
