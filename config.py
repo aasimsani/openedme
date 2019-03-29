47c0ba65b7c109f90800e52d2de16fe2ffaae106
Ubuntu <openedme@opened-me-server.pech1yhh424utb1s5jstn4e0jb.bx.internal.cloudapp.net>
2019-03-29 08:36:34 +0000

    Fixed and cleaned commits

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 # Configure this file to make the application work
 DOMAIN_NAME = "https://openedme.com"
-MJ_APIKEY_PRIVATE = "***REMOVED***"
-MJ_APIKEY_PUBLIC = "***REMOVED***"
-IPSTACK_KEY = "fe8ece2dfa3392b0e7246283c85ef669"
+MJ_APIKEY_PRIVATE = ""
+MJ_APIKEY_PUBLIC = ""
+IPSTACK_KEY = ""
